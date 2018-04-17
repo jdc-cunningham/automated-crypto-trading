d3f70d2567ab92716e156d489a97c4ca2ba12e26
Jacob David C Cunningham <greenace92@gmail.com>
2018-04-17 00:36:23 -0500

    python gdax websocket
    isolated price not much really

---
+++ 04-17-2018/gdax-websocket.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python
+
+# import websocket
+from websocket import create_connection
+import json
+import pickle
+import ast
+import time
+
+ws = create_connection("wss://ws-feed.gdax.com")
+
+ws_sub_obj = {}
+ws_sub_obj["type"] = "subscribe"
+ws_sub_obj["product_ids"] = ["BTC-USD"]
+ws_sub_obj["channels"] = ["ticker"]
+
+# what_is = pickle.load(ws_sub_obj)
+
+print "Subscribe to BTC-USD"
+ws.send(
+    # what_is
+    json.dumps(ws_sub_obj)
+    # {
+    #     "type": "subscribe",
+    #     "product_ids": [
+    #         "ETH-USD",
+    #         "ETH-EUR"
+    #     ],
+    #     "channels": [
+    #         "level2",
+    #         "heartbeat",
+    #         {
+    #             "name": "ticker",
+    #             "product_ids": [
+    #                 "ETH-BTC",
+    #                 "ETH-USD"
+    #             ]
+    #         }
+    #     ]
+    # }
+)
+# print "Sent"
+print "Receiving..."
+result =  ws.recv()
+print "Received '%s'" % result
+
+while True:
+    time.sleep(0.1)
+    result = ws.recv()
+    # print json.loads(result).price
+    ticker_obj = json.loads(result)
+    if 'channels' not in result:
+        print ticker_obj['price']
+
+# def output():
+#     print "Received '%s'" % result
+
+# while True:
+# ws.onmessage() = output
+# ws.close()
